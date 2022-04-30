6dc14b5c155eb91656d5af22f6d5c9dd3e664df9
rezwanrahimr <rezwanrahim69@gmail.com>
2022-04-30 14:27:03 +0600

    navbar added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img
+                            alt=""
+                            src="https://avatars.githubusercontent.com/u/4430336?s=200&v=4"
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{' '}
+                       ORACLE INVENTORY
+                    </Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
