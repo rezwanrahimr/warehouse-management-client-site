902ca8edd35ee2b152d6c71b82d51046c6aa296f
rezwanrahimr <rezwanrahim69@gmail.com>
2022-05-04 21:15:16 +0600

    404 added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/Home/Home'
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
-;
+import Error from './components/Error/Error';
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signup' element={<SignUp></SignUp>}></Route>
+        <Route path='*' element={<Error></Error>}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/Error/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <img src="https://stories.freepiklabs.com/storage/26838/oops-404-error-with-a-broken-robot-pana-2854.png" alt="" />
+        </div>
+    );
+};
+
+export default Error;
